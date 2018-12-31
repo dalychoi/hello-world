70fdc12a413a5711051f854f4435d4bd980059ff
dalychoi <dalychoi@gmail.com>
2018-12-31 21:23:55 +0900

    get json array

--- src/main/java/com/dalychoi/hello_world/App.java
+++ src/main/java/com/dalychoi/hello_world/App.java
@@ -1,6 +1,11 @@
 package com.dalychoi.hello_world;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.core.ParameterizedTypeReference;
 
 /**
  * Hello world!
@@ -8,10 +13,24 @@ import org.springframework.web.client.RestTemplate;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-    	RestTemplate restTemplate = new RestTemplate();
-    	Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-    	System.out.println(quote.toString());
-    }
+	public static void main( String[] args )
+	{
+		RestTemplate restTemplate = new RestTemplate();
+		Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+		System.out.println(quote.toString());
+		
+		// get json array
+		ResponseEntity<List<Post>> response = restTemplate.exchange(
+			"https://jsonplaceholder.typicode.com/posts?userId=1",
+			HttpMethod.GET,
+			null,
+			new ParameterizedTypeReference<List<Post>>() {}
+		);
+		List<Post> posts = response.getBody();
+		
+		System.out.println("Count of posts: " + posts.size());
+		for (Post post : posts) {
+			System.out.println(post.toString());
+		}
+	}
 }
--- src/test/java/com/dalychoi/hello_world/AppTest.java
+++ src/test/java/com/dalychoi/hello_world/AppTest.java
@@ -1,5 +1,8 @@
 package com.dalychoi.hello_world;
 
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import org.junit.Test;
@@ -7,15 +10,32 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.greaterThan;
+
+import java.util.List;
 
 public class AppTest 
 {
-    private RestTemplate restTemplate = new RestTemplate();
-	private Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-
 	@Test
 	public void testApp()
     {
+	    RestTemplate restTemplate = new RestTemplate();
+		Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
     	assertThat(quote.getValue(), is(notNullValue()));
     }
+	
+	@Test
+	public void testJsonArrayApp()
+	{
+	    RestTemplate restTemplate = new RestTemplate();
+		// get json array
+		ResponseEntity<List<Post>> response = restTemplate.exchange(
+			"https://jsonplaceholder.typicode.com/posts?userId=1",
+			HttpMethod.GET,
+			null,
+			new ParameterizedTypeReference<List<Post>>() {}
+		);
+		List<Post> posts = response.getBody();
+		assertThat(posts.size(), greaterThan(0));
+	}
 }
